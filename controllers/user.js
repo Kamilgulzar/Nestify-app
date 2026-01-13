87bb86e3b0d4c1d53ddf281f066f2da9d5f8e8be
Kamil Gulzar <kamilgulzar289@gmail.com>
2026-01-13 17:31:33 +0500

    basic changes after deployment

--- controllers/user.js
+++ controllers/user.js
@@ -27,7 +27,7 @@ module.exports.renderLoginForm = (req,res)=>{
 };
 
 module.exports.login = async(req,res)=>{
-  req.flash("success","Welcome back to Universa");
+  req.flash("success","Welcome back to Nestify!");
   const redirectUrl = res.locals.redirectUrl || "/listings"
   res.redirect(redirectUrl)
 }
